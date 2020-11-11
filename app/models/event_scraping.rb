f7e4e299bb6bb00d868006c781cd0da1d50449a6
Koji Yamamoto <koji@KojinoMacBook-puro.local>
2020-11-11 14:50:36 +0900

    小さな変更

--- app/models/event_scraping.rb
+++ app/models/event_scraping.rb
@@ -2,33 +2,6 @@ require 'mechanize'
 
 class EventScraping < ApplicationRecord
     def self.scrapeFirst
-        agent = Mechanize.new
-        agent.user_agent_alias = "Mac Safari"
-        page = agent.get("https://blueshipjapan.com/search/event/catalog")
-        elements = page.search('h2.event_title a')
-
-        elements.each do |element|
-            url = element.get_attribute(:href)
-            page = agent.get(url)
-            event_name = page.search('section.container h1').inner_text
-            event_owner = page.search('table a span').inner_text
-            event_date = page.search('div.event_datetime div.left p.value').inner_text
-
-            event = Event.new
-            event.name = event_name
-            event.owner = event_owner
-            event.date = event_date
-            event.site = "BlueShip"
-            if Owner.find_by(name: event.owner) then
-                event.link = true
-            else
-                event.link = false
-            end
-            event.save
-        end
-    end
-
-    def self.scrapeSecond
         for i in 0..45 do
             agent = Mechanize.new
             agent.user_agent_alias = "Mac Safari"
@@ -57,6 +30,33 @@ class EventScraping < ApplicationRecord
                 end
                 event.save
             end
+        end
+    end
+
+    def self.scrapeSecond
+        agent = Mechanize.new
+        agent.user_agent_alias = "Mac Safari"
+        page = agent.get("https://blueshipjapan.com/search/event/catalog")
+        elements = page.search('h2.event_title a')
+
+        elements.each do |element|
+            url = element.get_attribute(:href)
+            page = agent.get(url)
+            event_name = page.search('section.container h1').inner_text
+            event_owner = page.search('table a span').inner_text
+            event_date = page.search('div.event_datetime div.left p.value').inner_text
+
+            event = Event.new
+            event.name = event_name
+            event.owner = event_owner
+            event.date = event_date
+            event.site = "BlueShip"
+            if Owner.find_by(name: event.owner) then
+                event.link = true
+            else
+                event.link = false
+            end
+            event.save
         end
     end
 end
